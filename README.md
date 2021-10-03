e3121a15a507f54b4968008fba3a26a6f12e39ff
zpzyyzp <zpzyyzp@gmail.com>
2021-10-03 16:18:17 +0900

    add README.md fix contentLoop.js

---
+++ README.md
@@ -0,0 +1,14 @@
+# A Playwright Script
+
+## Build Setup
+```
+# install dependencies
+$ npm install # Or yarn install
+
+# scrape one page
+$ npm run start
+
+# scrape multi page
+$ npm run startAll
+```
+Edit index.js or contentLoop.js
--- contentLoop.js
+++ contentLoop.js
@@ -2,12 +2,12 @@ import playwright from 'playwright'
 import fs from 'fs'
 import getMeta from './libs/getMeta.js'
 
-async function fetchAll(baseUrl, count , file) {
+async function fetchAll(baseUrl, from, to, file) {
   const browser = await playwright.firefox.launch({ headless: false })
   const page = await browser.newPage()
 
-  for(let k = count; k > 0; k--) {
-    await page.goto(baseUrl + count)
+  for(let k = from; k <= to; k++) {
+    await page.goto(baseUrl + k)
     await page.waitForSelector('.mh-loop-item', { timeout: 30000})
     const content = await page.evaluate(() => {
       const articles = document.querySelectorAll('article .mh-loop-title a')
@@ -22,4 +22,4 @@ async function fetchAll(baseUrl, count , file) {
   await browser.close()
 }
 
-fetchAll('http://13dl.net/page/', 43, 'content')
+fetchAll('http://13dl.net/page/', 4, 35, 'content')
